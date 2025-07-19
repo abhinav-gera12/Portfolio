5893736cd0862084523ddffb0638776df3e0501d
unknown <abhinav.gera12@gmail.com>
2025-07-20 00:21:23 +0530

    updated the deployment script

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Portfolio/',
   plugins: [react()],
+  base: '/Portfolio/', // Set base for GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
